397c3d5f39f7d78227b0876d7dd6c1cf1380d9d5
AethersHaven <sullivandovie@gmail.com>
2022-02-22 21:18:47 -0500

    Indexing

---
+++ src/main/java/frc/robot/commands/superstructure/ballHandling/ReverseIndexing.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.superstructure.ballHandling;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ReverseIndexing extends CommandBase {
+
+    public ReverseIndexing() {
+        
+
+        
+    }
+    
+    @Override
+    public void execute() {
+
+
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return false;
+
+    }
+    
+}
