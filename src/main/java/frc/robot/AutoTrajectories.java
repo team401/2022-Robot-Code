5bdf5c2261930adeed518438e77af60734ebfdb1
Ayca <ayca@bulbul.net>
2022-02-24 08:46:49 -0500

    Autonomous setup

---
+++ src/main/java/frc/robot/AutoTrajectories.java
@@ -0,0 +1,83 @@
+package frc.robot;
+
+import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+
+public class AutoTrajectories {
+
+    /**
+     * Defines the trajectories to be run through auto
+     * 
+     * Full field length: 650 inches
+     * Full field width: 324 inches
+     * 
+     * (0,0) is the bottom left corner of the field when you are looking at it horizontally, 
+     * and blue drivers station is on the left
+     * 
+     * All of these positions assume that our robot is centered in the middle of the pentagon
+     */
+
+    // configures the maximum velocity and accel for the trajectories
+    private final static TrajectoryConfig config = 
+        new TrajectoryConfig(
+            AutoConstants.maxVelocityMetersPerSec, 
+            AutoConstants.maxAccelerationMetersPerSecondPerSecond
+        )
+        .setKinematics(DriveConstants.kinematics);
+    
+    //when looking from the the y-axis (blue driver station), it is the leftmost blue pentagon
+    //need to figure out how much to go backwards
+    public static Trajectory LeftBlue = 
+        TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.inchesToMeters(253), Units.inchesToMeters(189), new Rotation2d(0))
+            ), 
+            config
+    );
+
+    //when looking from the the y-axis (blue driver station), it is the rightmost blue pentagon
+    //need to figure out how much to go backwards
+    public static Trajectory RightBlue = 
+        TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.inchesToMeters(296.698), Units.inchesToMeters(90.876), new Rotation2d(0))
+            ), 
+            config
+    );
+
+    /*
+    when looking from the the y-axis (blue driver station), it is the lefttmost right pentagon 
+    (behind strucutre)
+    need to figure out how much to go backwards
+    */
+    public static Trajectory LeftRed =
+        TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.inchesToMeters(351.302), Units.inchesToMeters(233.124), new Rotation2d(0))
+            ), 
+            config
+    );
+
+    /*
+    when looking from the the y-axis (blue driver station), it is the righttmost right pentagon 
+    (behind strucutre)
+    need to figure out how much to go backwards
+    */
+    public static Trajectory RightRed =
+        TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.inchesToMeters(395.124), Units.inchesToMeters(134.698), new Rotation2d(0))
+            ), 
+            config
+    );
+
+}
---
+++ src/main/java/frc/robot/commands/autonomous/AutoRoutines.java
@@ -0,0 +1,119 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.AutoTrajectories;
+import frc.robot.commands.drivetrain.FollowTrajectory;
+import frc.robot.commands.superstructure.shooting.RampUp;
+import frc.robot.commands.superstructure.shooting.Shoot;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IndexingSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+public class AutoRoutines extends SequentialCommandGroup {
+    
+    public enum StartingPosition {
+        Left,
+        Right
+    }
+
+    public enum Side {
+        Blue,
+        Red
+    }
+
+    private final StartingPosition startingPosition;
+    private final Side side;
+
+    public AutoRoutines(
+        StartingPosition position, 
+        Side sideInputed, 
+        DriveSubsystem drive, 
+        IntakeSubsystem intake, 
+        IndexingSubsystem indexer, 
+        VisionSubsystem limelight, 
+        ShooterSubsystem shooter
+    ) {
+
+        startingPosition = position;
+        side = sideInputed;
+
+        drive.resetIMU();
+
+        //need to add the actual commands
+
+        switch (startingPosition) {
+
+            case Left:
+                
+                switch (side) {
+
+                    case Blue:
+
+                        drive.resetPose(AutoTrajectories.LeftBlue.getInitialPose());
+
+                        addCommands(
+                            new SequentialCommandGroup(
+                                new RampUp(shooter, Units.rotationsPerMinuteToRadiansPerSecond(4100)),
+                                new Shoot(shooter, indexer)
+                            ),
+                            new FollowTrajectory(drive, AutoTrajectories.LeftBlue)
+                        );
+
+                        break;
+                    
+                    case Red:
+ 
+                        drive.resetPose(AutoTrajectories.LeftRed.getInitialPose());
+
+                        addCommands(
+                            new ParallelCommandGroup(
+                                new Shoot(shooter, indexer)
+                            ),
+                            new FollowTrajectory(drive, AutoTrajectories.LeftRed)
+                        );
+
+                        break;
+
+                }
+
+            case Right:
+
+                switch (side) {
+
+                    case Blue:
+
+                        drive.resetPose(AutoTrajectories.RightBlue.getInitialPose());
+                        addCommands(
+                            new SequentialCommandGroup(
+                                new RampUp(shooter, Units.rotationsPerMinuteToRadiansPerSecond(4100)),
+                                new Shoot(shooter, indexer)
+                            ),
+                            new FollowTrajectory(drive, AutoTrajectories.RightBlue)
+                        );
+
+                        break;
+                
+                    case Red:
+
+                        drive.resetPose(AutoTrajectories.RightRed.getInitialPose());
+                            addCommands(
+                                new SequentialCommandGroup(
+                                    new RampUp(shooter, Units.rotationsPerMinuteToRadiansPerSecond(4100)),
+                                    new Shoot(shooter, indexer)
+                                ),
+                                new FollowTrajectory(drive, AutoTrajectories.RightRed)
+                            );
+
+                        break;
+                }
+
+            }
+        }
+
+
+
+}
