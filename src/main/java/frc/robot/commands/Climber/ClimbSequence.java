c3ae55801e5bb409e4a6905e090672660c0a7b48
AethersHaven <sullivandovie@gmail.com>
2022-02-28 18:17:44 -0500

    new climb sequence for testing

--- src/main/java/frc/robot/commands/Climber/ClimbSequence.java
+++ src/main/java/frc/robot/commands/Climber/ClimbSequence.java
@@ -1,34 +1,62 @@
 package frc.robot.commands.climber;
 
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.subsystems.RotationArmSubsystem;
 import frc.robot.subsystems.TelescopeArmSubsystem;
-import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.subsystems.RotationArmSubsystem.Mode;
 
 public class ClimbSequence extends SequentialCommandGroup {
 
-    /**
-     * 1: Driver manually extends telescope and lines up bot with 2nd bar
-     * 2: Driver presses button and activates climb sequence
-     * 3: Retract Telescope fully (0.5)
-     * 4: Rotate arms to straight up (0)
-     * 5: Extend Telescope 5 in
-     * 6: Rotate arms to 0.6
-     * 7: Extend Telescope fully
-     * 8: Rotate arms to 0.52 (unsure about measurement)
-     * 9: Retract Telescope fully
-     * 
-     */
-
     public ClimbSequence(TelescopeArmSubsystem telescope, RotationArmSubsystem rotation) {
 
         addCommands(
+
+            // Move rotation arm back to clear the bar
+            new UpdateRotationArm(rotation, ClimberConstants.defaultArmPosition, Mode.Climbing),
+
+            // Pull the robot up while holding the rotation arms back
+            new UpdateTelescopeArms(telescope, 0)
+                .raceWith(new HoldPositionRotationArms(rotation)),
+            
+            // Rotate the arms to catch the bar while holding the telescope
+            new UpdateRotationArm(rotation, 0.1, Mode.Climbing)
+                .raceWith(new HoldPositionTelescopeArms(telescope)),
+            
+            // Push the telescope arms up a little while holding the rotation arms
+            new UpdateTelescopeArms(telescope, 5)
+                .raceWith(new HoldPositionRotationArms(rotation)),
+            
+            // Rotate the arms to catch the traversal bar later while holding the telescope
+            new UpdateRotationArm(rotation, 0.6, Mode.Climbing)
+                .raceWith(new HoldPositionTelescopeArms(telescope)),
+
+            // Extend the telescope arms to catch the traversal bar later while holding the rotation arms
+            new UpdateTelescopeArms(telescope, 30)
+                .raceWith(new HoldPositionRotationArms(rotation)),
+
+            // Rotate the arms to hit the traversal bar while holding the telescope
+            new UpdateRotationArm(rotation, 0.52, Mode.Climbing)
+                .raceWith(new HoldPositionTelescopeArms(telescope)),
+
+            // Hook onto the traversal bar and pull up enough to get off the previous bar while holding the rotation arms
+            new UpdateTelescopeArms(telescope, 0)
+                .raceWith(new HoldPositionRotationArms(rotation)),
+
+            // Move rotation arm back to clear the bar
+            new UpdateRotationArm(rotation, ClimberConstants.defaultArmPosition, Mode.Climbing)
+                .raceWith(new HoldPositionTelescopeArms(telescope)),
+            
+            // Pull the robot up while holding the rotation arms back
+            new UpdateTelescopeArms(telescope, 0)
+                .raceWith(new HoldPositionRotationArms(rotation))
+            
+            // Hopefully at 3rd bar?
+            
+            /*
+            OLD CODE
             // Retract telescopes and stay there
             new UpdateRotationArm(rotation, ClimberConstants.defaultArmPosition, Mode.Climbing),
             new UpdateTelescopeArms(telescope, 0),
@@ -65,7 +93,7 @@ public class ClimbSequence extends SequentialCommandGroup {
             new UpdateTelescopeArms(telescope, 0.1)
             .raceWith(new HoldPositionRotationArms(rotation))
             //new HoldPositionTelescopeArms(telescope)
-            // Hopefully at 3rd bar?
+            // Hopefully at 3rd bar?*/
  
         );
 
